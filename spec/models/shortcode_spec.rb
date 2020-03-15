c5e6bc9b59b6ddaca096c22cba746e1027675ec4
Harvin Llanes <harvin.llanes@gmail.com>
2020-03-15 17:07:38 -0600

    Update test

--- spec/models/shortcode_spec.rb
+++ spec/models/shortcode_spec.rb
@@ -4,8 +4,10 @@ require 'faker'
 RSpec.describe Shortcode, :type => :model do
   context 'validation test' do
     it 'ensures code presence' do
-      sc = Shortcode.new(code: Faker::Alphanumeric.alpha(number: 4), url: Faker::Internet.domain_name.save
-      expect(user).to eq(false)
-    end
+      sc = Shortcode.new(code: Faker::Alphanumeric.alpha(number: 4), url: Faker::Internet.domain_name).save
+      expect(sc).to eq(true)
+      
+      sc1 = Shortcode.new(url: Faker::Internet.domain_name).save
+      expect(sc1).to eq(false)    end
   end
 end
