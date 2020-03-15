18278b29579e8b87961a7bb94b2bfe7f6a7945d1
Harvin Llanes <harvin.llanes@gmail.com>
2020-03-14 23:51:31 -0600

    Update occurrences

--- app/controllers/api/shortener_controller.rb
+++ app/controllers/api/shortener_controller.rb
@@ -2,6 +2,7 @@ class Api::ShortenerController < Api::BaseApiController
   def resolve
     site = Shortcode.find_by_code(params[:code])
     if site.present?
+      site.update_attributes(occurrences: (0 || site.occurrences))
       render json: {
         success: true,
 	url: site.url
