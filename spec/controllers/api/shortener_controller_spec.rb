f6758e77b7575c0ef0858cd79f1d4f73b11e0b4c
Harvin Llanes <harvin.llanes@gmail.com>
2020-03-16 08:38:27 -0600

    Add api test

--- spec/controllers/api/shortener_controller_spec.rb
+++ spec/controllers/api/shortener_controller_spec.rb
@@ -13,4 +13,37 @@ RSpec.describe Api::ShortenerController, type: :controller do
   
     expect(response.body).to include_json(success:true)
   end
+
+   Shortcode.create(code: 'abc123', url: 'www.google.com')
+
+  it 'Search record' do
+    get :resolve, params: { :code => 'abc123' }
+  
+    expect(response.body).to include_json(success:true)
+  end
+
+  it 'Search not existed record' do
+    get :resolve, params: { :code => 'xyaz123' }
+  
+    expect(response.body).to include_json(success:false)
+  end
+
+  it 'Create new record' do
+    post :create, params: { shortcode: { code: 'efg456', url: 'www.anyurl.com' } }
+    expect(response.body).to include_json(success:true)
+
+  end
+
+  it 'Create new record without url' do
+    post :create, params: { shortcode: { code: 'efg456'} }
+    expect(response.body).to include_json(success:false)
+  end
+
+  it 'Create new record without code' do
+    post :create, params: { shortcode: { url: 'www.anyurl.com'} }
+    expect(response.body).to include_json(success:false)
+
+  end
+
+
 end
